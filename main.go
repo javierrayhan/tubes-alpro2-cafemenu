9a01196435d18bc9f13bcd776bd4f1b8fd391334
Bagastama20007 <117610856+Bagastama20007@users.noreply.github.com>
2026-05-07 22:43:12 +0700

    menambahkan fitur pemilihan mode

--- main.go
+++ main.go
@@ -8,12 +8,37 @@ type menu struct {
 	kategori     string
 	harga        int
 	komposisi    string
-	ketersediaan bool //true = sedia,  false = tidak tersedia
+	ketersediaan bool
 }
 
 type arrMenu [999]menu
 
 func main() {
-	var n int
-	fmt.Scan(&n)
+	var mode int
+	fmt.Printf("%-4c %s\n", ' ', "Cafe Menu")
+	fmt.Println("-------------------")
+	fmt.Println("1. Admin")
+	fmt.Println("2. Pelanggan")
+	fmt.Println("3. Exit")
+	fmt.Scan(&mode)
+	if mode == 1 {
+		adminMode()
+	} else if mode == 2 {
+		UserMode()
+	} else if mode == 3 {
+		Exit()
+	}
+}
+
+func adminMode() {
+
+}
+
+func UserMode() {
+
+}
+
+func Exit() {
+	fmt.Println("")
+	fmt.Println("Terima kasih telah menggunakan layanan kami!")
 }

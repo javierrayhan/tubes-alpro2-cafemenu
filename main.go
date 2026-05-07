6a0f38264a2cd36b8a16ac778ba752ac4ed44821
Bagastama20007 <117610856+Bagastama20007@users.noreply.github.com>
2026-05-07 22:29:03 +0700

    cek

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
+import "fmt"
+
 type menu struct {
-	id int
-	nama string
-	kategori string
-	harga int
-	komposisi string
-	ketersediaan bool 	//true = sedia,  false = tidak tersedia
+	id           int
+	nama         string
+	kategori     string
+	harga        int
+	komposisi    string
+	ketersediaan bool //true = sedia,  false = tidak tersedia
 }
 
 type arrMenu [999]menu
 
-func main(){
-
+func main() {
+	var n int
+	fmt.Scan(&n)
 }
